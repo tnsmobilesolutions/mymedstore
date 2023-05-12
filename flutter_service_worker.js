66b51dc28d292a1e0e519f538a5ad35975923885
Amrutanshu-Chunu <amrutanshup@gmail.com>
2023-05-12 10:15:00 +0530

    new build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "47f97da0979d8b34bcf006b37b69bb26",
-"index.html": "e1e9475e306e6a32b4af7c2313dbca2d",
-"/": "e1e9475e306e6a32b4af7c2313dbca2d",
-"main.dart.js": "8e1355dcaf1de310cb32c8551bdeb1fd",
+"index.html": "3c0dea71ecafb10e5f14f1128bf9f3e9",
+"/": "3c0dea71ecafb10e5f14f1128bf9f3e9",
+"main.dart.js": "542c69f864f6ae6ab14dfd1bf7b9193f",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
