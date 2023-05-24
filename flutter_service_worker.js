40881bfdceeed1e3828c06e4b0b611ed59526955
Amrutanshu-Chunu <amrutanshup@gmail.com>
2023-05-24 18:17:11 +0530

    back button

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "47f97da0979d8b34bcf006b37b69bb26",
-"index.html": "4074663e35305d5ebea6e70a50655449",
-"/": "4074663e35305d5ebea6e70a50655449",
-"main.dart.js": "03259fc58b5274cb6da4542240b51dbd",
+"index.html": "e8423f8b01869f57da17d27747fa16fb",
+"/": "e8423f8b01869f57da17d27747fa16fb",
+"main.dart.js": "9397b370a2d4cdb281c1864dfcc308ca",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
