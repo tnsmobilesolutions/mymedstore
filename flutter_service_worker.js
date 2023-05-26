635404f96501b22e01a5466d645de3e9a0b9ec36
Amrutanshu-Chunu <amrutanshup@gmail.com>
2023-05-26 20:18:06 +0530

    back button in filtersort

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "47f97da0979d8b34bcf006b37b69bb26",
-"index.html": "207179e9e1c1c75ab6ce214d915df0ee",
-"/": "207179e9e1c1c75ab6ce214d915df0ee",
-"main.dart.js": "ebc3730f4ed30398ca5038b656881600",
+"index.html": "c6dbf70b314fe83859c4312ce44a6dca",
+"/": "c6dbf70b314fe83859c4312ce44a6dca",
+"main.dart.js": "b940f684a7daff15068022ab97bdc836",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
