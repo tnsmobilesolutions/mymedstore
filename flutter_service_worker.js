1d0107409fbe06fcb691feb2e16f6e133ff874fc
Amrutanshu-Chunu <amrutanshup@gmail.com>
2023-06-15 03:32:55 +0530

    port change

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "47f97da0979d8b34bcf006b37b69bb26",
-"index.html": "eb363a193c6060766832219e0a591e8c",
-"/": "eb363a193c6060766832219e0a591e8c",
-"main.dart.js": "0814888b0f771bb69419a7a789e37b25",
+"index.html": "90bda298a8f923a14c0be402d87ef569",
+"/": "90bda298a8f923a14c0be402d87ef569",
+"main.dart.js": "7da1eccfbff622ca1eff8567f89247e0",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
