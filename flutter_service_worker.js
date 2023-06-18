168c2f1e971912ba2f432c3c1e459516732fd70d
Amrutanshu-Chunu <amrutanshup@gmail.com>
2023-06-18 17:10:57 +0530

    remote config in signup

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "47f97da0979d8b34bcf006b37b69bb26",
-"index.html": "3e561a6c6c038467aea35c58f735ba40",
-"/": "3e561a6c6c038467aea35c58f735ba40",
-"main.dart.js": "07867085717c397590bfff1e8e305a60",
+"index.html": "6a4046b0a37ad2b6a72c34197dded883",
+"/": "6a4046b0a37ad2b6a72c34197dded883",
+"main.dart.js": "5d2897991c9ee3dd1bbea64a110bb4d7",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
